74fc9c33b8b7130c82e382af63f90b09e12e7259
Amil Jabarli <amiljabbarlee@gmail.com>
2025-05-03 15:46:25 -0500

    feat: integrate Ant Design components and enhance InputForm styling
    - Added Ant Design dependency to package.json.
    - Replaced textarea with Ant Design's TextArea component in InputForm.
    - Updated InputForm styles to enforce fixed height for the textarea.
    - Added a new explanation level label with specific styling.
    - Changed the submit button to use Ant Design's Button component.

--- src/components/ui/InputForm/index.tsx
+++ src/components/ui/InputForm/index.tsx
@@ -2,6 +2,9 @@
 
 import styles from "./index.module.css";
 import LevelSelector from "../LevelSelector";
+import { Button, Input, Typography } from "antd";
+
+const { TextArea } = Input;
 
 export default function InputForm() {
   const level = "5-year-old";
@@ -9,17 +12,18 @@ export default function InputForm() {
   const setLevel = (val: string) => console.log(val);
   return (
     <div className={`${styles.formWrapper}`}>
-      <textarea
+      <TextArea
         rows={8}
         placeholder="Paste your text here..."
         className={styles.textarea}
       />
       <div className={styles.levelSelectorWrapper}>
+        <p className={styles.explanationLevelLabel}>Select explanation level</p>
         <LevelSelector selected={level} onChange={setLevel} />
       </div>
-      <button type="submit" className={styles.button}>
+      <Button className={styles.button} size="large">
         Explain it
-      </button>
+      </Button>
     </div>
   );
 }
