eacb7fab814af4a10aa58a30ffbf05952b80d165
Amil Jabarli <amiljabbarlee@gmail.com>
2025-05-02 19:10:39 -0500

    style: Adjust InputForm and LevelSelector styles for improved layout

--- src/components/ui/InputForm/index.tsx
+++ src/components/ui/InputForm/index.tsx
@@ -14,19 +14,18 @@ export default function InputForm() {
 
   const setLevel = (val: string) => console.log(val);
   return (
-      <div className={`${styles.formWrapper} ${inter.className}`}>
-        <textarea
-          rows={8}
-          placeholder="Paste your text here..."
-          className={styles.textarea}
-        />
-        <div className={styles.levelSelectorWrapper}>
-          <label className={styles.title}>Choose your explanation level</label>
-          <LevelSelector selected={level} onChange={setLevel} />
-        </div>
-        <button type="submit" className={styles.button}>
-          Explain it
-        </button>
+    <div className={`${styles.formWrapper}`}>
+      <textarea
+        rows={8}
+        placeholder="Paste your text here..."
+        className={styles.textarea}
+      />
+      <div className={styles.levelSelectorWrapper}>
+        <LevelSelector selected={level} onChange={setLevel} />
       </div>
+      <button type="submit" className={styles.button}>
+        Explain it
+      </button>
+    </div>
   );
 }
