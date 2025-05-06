f8f103b98ba6baa14d5f9708ff2a765ca841319f
Amil Jabarli <amiljabbarlee@gmail.com>
2025-05-06 12:16:55 -0500

    feat: update InputForm to manage state and handle explanations; add new dependencies for Ant Design icons and patch

--- src/components/ui/InputForm/index.tsx
+++ src/components/ui/InputForm/index.tsx
@@ -1,29 +1,69 @@
 "use client";
 
+import React, { useState } from "react";
 import styles from "./index.module.css";
 import LevelSelector from "../LevelSelector";
-import { Button, Input, Typography } from "antd";
+import { Button, Input, Tag } from "antd";
 
 const { TextArea } = Input;
 
 export default function InputForm() {
-  const level = "5-year-old";
+  const [levelSelected, setLevelSelected] = useState<string>("5-year-old");
+  const [paragraph, setParagraph] = useState<string>("");
+  const [alertMessageText, setAlertMessageText] = useState<string | null>(null);
+  const [alertColor, setAlertColor] = useState<"green" | "red" | "blue">(
+    "green"
+  );
 
-  const setLevel = (val: string) => console.log(val);
+  const setLevel = (level: string) => {
+    setLevelSelected(level);
+    console.log("New level: ", levelSelected);
+  };
+
+  const handleExplain = (level: string, text: string) => {
+    setAlertMessageText("Processing your request...");
+    setAlertColor("blue"); // Blue for processing
+
+    if (!level || !text) {
+      setAlertMessageText("Please provide all the information...");
+      setAlertColor("red"); // Red for error
+      return;
+    }
+
+    // Simulate a successful process
+    setTimeout(() => {
+      setAlertMessageText("Explanation generated successfully!");
+      setAlertColor("green"); // Green for success
+    }, 2000);
+
+    console.log({
+      text: paragraph,
+      level: levelSelected,
+    });
+  };
+
   return (
     <div className={`${styles.formWrapper}`}>
-      <TextArea
+      <textarea
         rows={8}
         placeholder="Paste your text here..."
+        value={paragraph}
+        onChange={(e) => setParagraph(e.target.value)}
         className={styles.textarea}
       />
       <div className={styles.levelSelectorWrapper}>
         <p className={styles.explanationLevelLabel}>Select explanation level</p>
-        <LevelSelector selected={level} onChange={setLevel} />
+        <LevelSelector selected={levelSelected} onChange={setLevel} />
       </div>
-      <Button className={styles.button} size="large">
+      <button
+        className={styles.button}
+        onClick={() => handleExplain(levelSelected, paragraph)}
+      >
         Explain it
-      </Button>
+      </button>
+      {alertMessageText && (
+        <p style={{ color: alertColor }}>{alertMessageText}</p>
+      )}
     </div>
   );
 }
